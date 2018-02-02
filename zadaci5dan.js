365a985807b65d972d6a255f904092a2e6c5967e
GIT student <classroom@bgit.rs>
2018-02-02 12:42:49 +0100

    prvi zadatak

---
+++ zadaci5dan.js
@@ -0,0 +1,13 @@
+/*var a = 3;
+var b = 5;
+var c = 10;
+
+if (a > b && a > c) { 
+    console.log(a);
+} else  if (b > a && b > c) {
+    console.log(b);
+} else if (c > a && c > b) {
+    console.log(c);
+}
+
+*/
