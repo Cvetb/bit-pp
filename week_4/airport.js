e66a1886421a08af8737084288edd4a3b9b77974
Branka Cvetic <cveticbranka@yahoo.com>
2018-02-24 11:21:13 +0100

    tidying_up

--- week_4/airport.js
+++ week_4/airport.js
@@ -3,48 +3,50 @@
 
     console.log('Hi!');
 
-
     function Person(name, surname) {
         this.name = name;
         this.surname = surname;
+
         this.getData = function () {
-
-            return this.name + " " + this.surname;
+            return this.name + ' ' + this.surname;
         }
-
     }
 
-    function Seat(number, category) {
-        this.number = number;
-        this.category = category;
+    function Seat(category) {
+        this.category = category.toUppercase || 'E';
+        this.number = (100 - 10) * Math.random() + 10;
 
-        number = Math.random() * (10 - 99) + 10;
         this.getData = function () {
-            return this.number.toString + ", " + this.category;
-
+            return this.number.toString + ', ' + this.category;
         }
     }
 
     function Passenger(person, seat) {
         this.person = person;
         this.seat = seat;
+
         this.getData = function () {
-
-            return this.seat + this.object;
+            return this.seat.getData() + this.person.getData();
         }
-
     }
 
     function Flight(relation, date) {
-
         this.list = [];
-        this.date = date;
+        this.date = new Date(date);
         this.relation = relation;
 
-        this.getData = function () {
-            return this.date + " " + this.relation + '\n\t\t' + ;
-
+        this.fDate = function () {
+            return this.date.getData() + '. ' + this.date.getMonth() + '. ' + this.date.getFullYear();
         }
+
+        this.getData = function () {
+            var l = ' ';
+            for (var i = 0; i < this.list.length; i++)
+                var m = this.list[i];
+            l += '\n\t\t' + m.getData();
+        }
+
+        return this.fdate() + ' ' + this.relation + l;
 
         this.addPassenger = function (passenger) {
             this.list.push(passenger);
@@ -52,19 +54,33 @@
         }
     }
 
-    function Airport(name) {
+    function Airport() {
+        this.name = 'Nikola Tesla'
         this.listFlights = [];
-        this.addFlight = function () {
 
-        }
+        this.addFlight = function (flight) {
+            this.listFlights.push(flight);
+        };
+
+        this.getData = function () {
+            var air = '';
+            var counter = 0;
+            for (var i = 0; i < this.listFlights; i++) {
+                var flight = this.listFlights[i];
+                counter += flight.list.lenght;
+                air += '\n\t' + flight.getData();
+            }
+            return 'Airport: ' + this.name + ', ' + 'total passengers' + counter + ' ' + air;
+        };
 
     }
-function createFlight( relation, date){
-    return new Flight (relation, date);
-}
 
-function createPassenger (name, surname, number, category){
-return new Passenger (name, surname,number, category);
-}
+    function createFlight(relation, date) {
+        return new Flight(relation, date);
+    }
+
+    function createPassenger(name, surname, number, category) {
+        return new Passenger(name, surname, number, category);
+    }
 
 })();
