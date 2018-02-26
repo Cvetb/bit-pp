50a4276a61ec50816a004734a4aa0da2ddb8a1c8
Branka Cvetic <cveticbranka@yahoo.com>
2018-02-26 20:47:23 +0100

    fest

--- week_4/festival.js
+++ week_4/festival.js
@@ -14,86 +14,56 @@
     function Movie(title, length, genre) {
         this.title = title;
         this.length = parseInt(length);
-        this.genre = genre;
         this.getData = function () {
-            return this.title + ' , ' + this.length + ' min, ' + this.genre.getData();
+            return this.title + ' , ' + this.length + ' min, ' + genre.getData();
         }
     }
 
     function Program(date) {
         this.listOfMovies = [];
         this.date = new Date(date);
-        this.movieCounter = 0;
-
+        this.totalNumOfMovies = 0;
         this.addMovie = function (movie) {
-            var counter = 0;
-            this.listOfMovies.forEach(function (item) {
-                if (item.genre == movie.genre) {
-                    counter++;
-                }
-
-            });
-            (movie);
-            if (this.totalLength() + movie.length <= 480 && counter < 4) {
-                this.listOfMovies.push(movie);
-                this.movieCount++;
-
-            } else if (this.totalLength() + movie.length > 480) {
-                console.log('Too long!');
-            } else {
-                console.log('There are already 4 movies of this genre in this program!');
+            this.listOfMovies.push(movie);
+            this.totalNumOfMovies++;
+        }
+        this.getData = function () {
+            var lengthOfAll = 0;
+            var dataAboutMovies = '';
+            for (var i = 0; i < this.listOfMovies.length; i++) {
+                lengthOfAll += parseInt(this.listOfMovies[i].length);
+                dataAboutMovies += this.listOfMovies[i].getData() + '\n';
             }
 
-            this.totalLength = function () {
-                var m = 0;
-                this.list.forEach(function (movie, i) {
-                    m += movie.length;
-                });
-                return m;
-            }
 
-            this.getData = function () {
-                var s = '\t' + this.date.getDate() + ' ' + (this.date.getMonth() + 1) + ' ' + this.date.getFullYear() + ', program duration ' + this.totalLength() + 'min';
-                this.list.forEach(function (movie, i) {
-                    s += '\n\t\t' + movie.getData();
-                });
-                return s;
-            }
+            return this.date + ', program duration ' + lengthOfAll + '\n' + dataAboutMovies;
+
         }
 
     }
 
-    function Festival(nameFest, numOfMovies) {
+
+    function Festival(nameFest) {
         this.listOfPrograms = [];
         this.nameFest = nameFest;
-        this.numOfMovies = parseInt(numOfMovies);
-        this.totalMovieCount = function () {
-            var n = 0;
-            this.listOfPrograms.forEach(function (program, i) {
-                n += program.movieCount;
-            });
-            return n;
-        }
+        this.numAllPr = 0;
         this.addProgram = function (program) {
-            if ((this.totalMovieCount() + program.movieCount) <= this.numOfMovies) {
-                this.listOfPrograms.push(program);
-            } else {
-                console.log('Maximum number of allowed movies exceeded!');
-            }
-
+            this.listOfPrograms.push(program);
+            this.numAllPr++;
         }
         this.getData = function () {
+            var numOfAllMovies = 0;
+            var allPrListed = '';
 
+            for (var i = 0; i < this.listOfPrograms.length; i++) {
+                numOfAllMovies += this.listOfPrograms[i].totalNumOfMovies;
+                allPrListed += this.listOfPrograms[i].getData() + '\n';
+
+            }
+
+            return this.nameFest + ' has ' + numOfAllMovies + ' movie titles \n' + allPrListed;
         }
-        if (this.listOfPrograms.length != 0) {
-            var s = this.nameFest + ' has ' + this.totalMovieCount() + ' movie titles';
-            this.listOfPrograms.forEach(function (program, i) {
-                s += '\n' + program.getData();
-            });
-        } else {
-            var s = this.nameFest + '\n\t Program to be announced';
-        }
-        return s;
+
     }
 
     var action = new Genre("Action");
@@ -113,7 +83,7 @@ var movie5 = new Movie('Deadpool',108, comedy);
     function createProgram(date) {
         return new Program(date);
     }
-    var program1 = new Program('28.10.2017');
+    var program1 = new Program('25.10.2017');
     var program2 = createProgram('28.10.2017');
 
     var fest1 = new Festival('Weekend festival');
